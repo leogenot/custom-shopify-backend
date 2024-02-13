80554404351df51bb467fa608d7fe635e3b52951
Léo Genot <leogenot@gmail.com>
2024-02-13 11:56:40 +0100

    wrking both ways now, if shopify id is not set but product in craft and shopify have the same handle then assign the id to the craft one making it linked to the shopify one

--- plugins/craft-shopify/src/jobs/SyncProduct.php
+++ plugins/craft-shopify/src/jobs/SyncProduct.php
@@ -1,61 +1,44 @@
 <?php
 
-/**
- * craft-shopify module for Craft CMS 3.x
- *
- * @link      https://leocompany.com
- * @copyright Copyright (c) 2021 One Design Company
- */
-
-
 namespace leo\craftshopify\jobs;
-
 
 use Craft;
 use craft\helpers\Json;
 use craft\queue\BaseJob;
 use Exception;
 use leo\craftshopify\CraftShopify;
-use leo\craftshopify\elements\Product;
+use craft\elements\Entry;
 
-/**
- * @author    One Design Company
- * @package   craft-shopify
- * @since     1.0.0
- */
 class SyncProduct extends BaseJob {
-    /**
-     * @var array
-     */
     public $productData = null;
 
-    /**
-     * @inheritdoc
-     */
     public function execute($queue): void {
         if (!$this->productData) {
             throw new Exception('Product Data is required.');
         }
 
         $shopifyId = $this->productData['id'];
-        $product = CraftShopify::$plugin->product->getProductModel($shopifyId);
-        CraftShopify::$plugin->product->populateProductModel($product, $this->productData);
-        $entry = CraftShopify::$plugin->product->updateEntry($this->productData);
-        if (Craft::$app->getElements()->saveElement($product) && $entry) {
-            $queue->setProgress(100);
-        } else {
+        $shopifyHandle = $this->productData['handle'];
 
-            throw new Exception('Unknown error saving product ' . $shopifyId);
+        // Get the Craft entry by handle
+        // $entry = Craft::$app->getEntries()->getEntryByHandle($shopifyHandle);
+        $entry = Entry::find()->section('products')->slug([$shopifyHandle])->one();
+
+        if (!$entry) {
+            throw new Exception('Craft entry with handle ' . gettype($shopifyId) . ' not found.');
         }
 
-        if ($product->getErrors()) {
-            throw new Exception('Product ' . $shopifyId . ' - ' . Json::encode($product->getErrors()));
+        // Update the Shopify ID field in the Craft entry
+        $entry->setFieldValue('shopifyId', (string)$shopifyId);
+
+        // Save the Craft entry
+        if (Craft::$app->getElements()->saveElement($entry)) {
+            $queue->setProgress(100);
+        } else {
+            throw new Exception('Unknown error saving product ' . $shopifyId);
         }
     }
 
-    /**
-     * @inheritdoc
-     */
     protected function defaultDescription(): string {
         return Craft::t('craft-shopify', "Sync Product ID {$this->productData['id']} from Shopify");
     }
